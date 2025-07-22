9da7ef7e972a3e5d8d9c7c5118cb0c9f0c225b2a
shafiahmed <shafiahmed120@gmail.com>
2025-07-23 00:09:07 +0530

    implemented caching

--- zapURL.Api/Services/UrlService/UrlService.cs
+++ zapURL.Api/Services/UrlService/UrlService.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using ErrorOr;
+using Microsoft.Extensions.Caching.Distributed;
 using zapURL.Api.Data.Repositories.ShortenUrlRepository;
 using zapURL.Api.Models;
 using zapURL.Api.Utilities;
@@ -7,25 +9,46 @@ namespace zapURL.Api.Services.UrlService;
 
 internal class UrlService : IUrlService
 {
+    private readonly IDistributedCache _cache;
     private readonly ILogger<UrlService> _logger;
     private readonly IShortUrlRepository _shortUrlRepository;
 
-    public UrlService(IShortUrlRepository shortUrlRepository, ILogger<UrlService> logger)
+    public UrlService(IShortUrlRepository shortUrlRepository, ILogger<UrlService> logger, IDistributedCache cache)
     {
         _shortUrlRepository = shortUrlRepository;
         _logger = logger;
+        _cache = cache;
     }
 
     public async Task<ErrorOr<string>> GetOriginalUrlAsync(string code)
     {
+        var cachedResult = await _cache.GetStringAsync(code);
+
+        if (cachedResult is not null) return cachedResult;
+
         var result = await _shortUrlRepository.GetOriginalUrlAsync(code);
-        return result.IsError ? result.Errors : result.Value;
+
+        if (result.IsError) return result.Errors;
+
+        await _cache.SetStringAsync(code, result.Value);
+
+        return result.Value;
     }
 
     public async Task<ErrorOr<List<ShortUrl>>> GetShortUrlsAsync()
     {
+        var cachedResult = await _cache.GetStringAsync("All");
+
+        if (cachedResult is not null) return JsonSerializer.Deserialize<List<ShortUrl>>(cachedResult);
+
         var result = await _shortUrlRepository.GetAllShortUrlsAsync();
-        return result.IsError ? result.Errors : result.Value;
+
+        if (result.IsError) return result.Errors;
+
+        await _cache.SetStringAsync("All", JsonSerializer.Serialize(result.Value),
+            new DistributedCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(20) });
+
+        return result.Value;
     }
 
     public async Task<ErrorOr<string>> ShortenUrlAsync(string originalUrl)
@@ -40,6 +63,7 @@ internal class UrlService : IUrlService
         if (result.IsError) return result.Errors;
         _logger.LogInformation("created short url with code: {Code} and originalURl: {OriginalUrl}", result.Value,
             originalUrl);
+        await _cache.RemoveAsync("All");
         return result.Value;
     }
 
@@ -49,6 +73,7 @@ internal class UrlService : IUrlService
         if (result.IsError) return result.Errors;
         _logger.LogInformation("updated short url with Id: {Id} code: {Code} and originalURl: {Original}", id,
             result.Value, originalUrl);
+        await _cache.RemoveAsync("All");
         return result.Value;
     }
 
@@ -57,6 +82,8 @@ internal class UrlService : IUrlService
         var result = await _shortUrlRepository.DeleteAsync(id);
         if (result.IsError) return result.Errors;
         _logger.LogInformation("Deleted short url with Id: {Id} and code: {Code}", id, result.Value);
+        await _cache.RemoveAsync("All");
+        await _cache.RemoveAsync(result.Value);
         return result.Value;
     }
 }
